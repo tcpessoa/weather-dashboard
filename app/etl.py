8d34932054215f16ff0da49b973e26908905c697
Tiago Pessoa <70546713+tcpessoa@users.noreply.github.com>
2023-08-11 21:51:48 +0400

    styles: fix naming conventions

--- app/etl.py
+++ app/etl.py
@@ -1,44 +1,42 @@
-import requests
-import os
 import logging
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-from sqlalchemy.orm import sessionmaker
-from app.config import DATABASE_URL
-from . import models
+import os
 
+import requests  # type: ignore
+from dotenv import load_dotenv  # type: ignore
+from sqlalchemy import create_engine  # type: ignore
+from sqlalchemy.orm import sessionmaker  # type: ignore
+
+from app.config import DATABASE_URL
+
+from . import models
 
 load_dotenv()
 
-Location = {
-    'lat': -1.292066,
-    'lon': 36.821945,
-    'appid': os.getenv("WEATHER_API")
-    }
 
 engine = create_engine(DATABASE_URL)
-Session = sessionmaker(autocommit=False, autoflush=False,bind=engine)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def extract_weather_data(Location):
+
+def extract_weather_data(location):
     """_summary_
 
     Args:
         Location (_type_): dict with the lat,lon and appid to a location
 
     Returns:
-        Json data 
+        Json data
     """
     try:
-        url= "https://api.openweathermap.org/data/2.5/weather?"
-        response = requests.get(url,params=Location)
-        weather_data = response.json() 
+        url = "https://api.openweathermap.org/data/2.5/weather?"
+        response = requests.get(url, params=location)
+        weather_data = response.json()
         return weather_data
     except Exception as e:
         logging.error(f"fetching weather data failed {e}")
-        return None 
-    
+        return None
 
-def transform_weather_data(weather_data):
+
+def transform_weather_data(weather_data: dict):
     """_summary_
 
     Args:
@@ -48,19 +46,30 @@ def transform_weather_data(weather_data):
         exctract data from the json
     """
     try:
-        location=weather_data["name"],
-        longitude=weather_data["coord"]["lon"],
-        latitude=weather_data["coord"]["lat"],
-        temperature=weather_data["main"]["temp"],
-        humidity=weather_data["main"]["humidity"],
-        pressure=weather_data["main"]["pressure"],
-        weather_description=weather_data["weather"][0]["description"]
-        return location, longitude, latitude, temperature, humidity, pressure, weather_description
-    except Exception as e:
-        logging.error("transforming weather data failed")
-    
+        location = (weather_data["name"],)
+        longitude = (weather_data["coord"]["lon"],)
+        latitude = (weather_data["coord"]["lat"],)
+        temperature = (weather_data["main"]["temp"],)
+        humidity = (weather_data["main"]["humidity"],)
+        pressure = (weather_data["main"]["pressure"],)
+        weather_description = weather_data["weather"][0]["description"]
+        return (
+            location,
+            longitude,
+            latitude,
+            temperature,
+            humidity,
+            pressure,
+            weather_description,
+        )
+    except KeyError as e:
+        logging.error(f"Error transforming weather data: {e}")
+        return None
 
-def load_weather_data_to_db(location,longitude,latitude,temperature, humidity, pressure, weather_description):
+
+def load_weather_data_to_db(
+    location, longitude, latitude, temperature, humidity, pressure, weather_description
+):
     """_summary_
 
     Args:
@@ -73,23 +82,48 @@ def load_weather_data_to_db(location,longitude,latitude,temperature, humidity, p
         weather_description (string): description of the current weather condition
     """
     try:
-        db_session=Session()
-        weather_data_to_db = models.Weather(location=location,longitude=longitude,latitude=latitude,temperature=temperature, humidity=humidity,pressure=pressure, weather_description=weather_description)
+        db_session = Session()
+        weather_data_to_db = models.Weather(
+            location=location,
+            longitude=longitude,
+            latitude=latitude,
+            temperature=temperature,
+            humidity=humidity,
+            pressure=pressure,
+            weather_description=weather_description,
+        )
         db_session.add(weather_data_to_db)
         db_session.commit()
         db_session.refresh(weather_data_to_db)
         print("Weather data saved to database")
-       
-    except Exception as e:  
+
+    except Exception as e:
         logging.error(f"Error loading weather data to database: {e}")
 
+
 def run_etl():
-    """_summary_
-    
-    """
-    weather_data = extract_weather_data( Location)
+    """_summary_"""
+    location = {"lat": -1.292066, "lon": 36.821945, "appid": os.getenv("WEATHER_API")}
+    weather_data = extract_weather_data(location)
     if weather_data:
-        location,longitude,latitude,temperature, humidity,pressure, weather_description = transform_weather_data(weather_data)
-        load_weather_data_to_db(location,longitude,latitude,temperature, humidity,pressure, weather_description)
+        (
+            location,
+            longitude,
+            latitude,
+            temperature,
+            humidity,
+            pressure,
+            weather_description,
+        ) = transform_weather_data(weather_data)
+        load_weather_data_to_db(
+            location,
+            longitude,
+            latitude,
+            temperature,
+            humidity,
+            pressure,
+            weather_description,
+        )
     else:
         logging.error("No weather data to load to database")
+
