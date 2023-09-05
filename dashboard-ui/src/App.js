44c9050d0df49c81deb20b2957589c0813de51a7
Tiago Pessoa <70546713+tcpessoa@users.noreply.github.com>
2023-09-05 21:48:34 +0400

    feat: connect dashboard to backend

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -1,73 +1,21 @@
 import './App.css';
-import BarChart from './components/BarChart';
-import LineChart from './components/LineChart';
-import SampleHello from './components/SampleHello';
 import { fetchData } from './services/Api';
 import { useEffect, useState } from "react"
-import SampleTempChart from './components/SampleTempChart';
+import WeatherChart from './components/WeatherChart';
 
 function App() {
-  const [users, setUsers] = useState([])
-  const [loadingUsers, setLoadingUsers] = useState(false)
-  const [loadingWeather, setLoadingWeather] = useState(false)
-  const [weatherData, setWeatherData] = useState([])
+  const [data, setData] = useState(undefined)
   useEffect(() => {
-    setLoadingUsers(true)
-    setLoadingWeather(true)
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(json => setUsers(json))
-      .finally(() => {
-        setLoadingUsers(false)
-      })
     fetchData()
       .then(data => {
-        setWeatherData(data)
-      })
-      .finally(() => {
-        setLoadingWeather(false)
+        setData(data)
       })
   }, [])
 
-  const data = {
-    xAxis: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    yAxis: [820, 932, 901, 934, 1290, 1330, 1320],
-  }
-
   return (
     <div className="App">
-      {loadingUsers ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <h1>Users</h1>
-          <table border={1}>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-            </tr>
-            {users.map(user => (
-              <tr key={user.id}>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                <td>{user.phone}</td>
-              </tr>
-            ))}
-          </table>
-        </>
-      )}
-      <SampleHello data={{ name: 'Tum' }} />
-      <div>
-        {loadingWeather ? (
-          <h2 style={{ color: 'black' }}>Loading...</h2>
-        ) : (
-          <LineChart data={weatherData} />
-        )}
-
-      </div>
-      <BarChart data={data} />
-      <SampleTempChart />
+      <h1>Dashboard UI</h1>
+      { data && <WeatherChart data={data} /> }
     </div>
   );
 }
---
+++ dashboard-ui/src/components/WeatherChart.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import ReactECharts from "echarts-for-react";
+
+function WeatherChart({ data }) {
+
+  const option = {
+    title: {
+      text: "Weather Data",
+    },
+    tooltip: {
+      trigger: "axis",
+    }, legend: {
+      data: ["Temperature", "Humidity", "Pressure"],
+    },
+    xAxis: {
+      type: "category",
+      data: data.items.map((item) => item.timestamp),
+    }, yAxis: [
+      {
+        type: "value",
+        name: "Temperature (K)",
+        min: Math.min(...data.items.map((item) => item.temperature)) - 10,
+        max: Math.max(...data.items.map((item) => item.temperature)) + 10,
+      },
+      {
+        type: "value",
+        name: "Humidity (%)",
+        min: 0,
+        max: 100,
+      },
+      {
+        type: "value",
+        name: "Pressure (hPa)",
+        min: Math.min(...data.items.map((item) => item.pressure)) - 10,
+        max: Math.max(...data.items.map((item) => item.pressure)) + 10,
+      },
+    ],
+    series: [
+      {
+        name: "Temperature",
+        type: "line",
+        data: data.items.map((item) => item.temperature),
+        yAxisIndex: 0,
+      },
+      {
+        name: "Humidity",
+        type: "line",
+        data: data.items.map((item) => item.humidity),
+        yAxisIndex: 1,
+      },
+      {
+        name: "Pressure",
+        type: "line",
+        data: data.items.map((item) => item.pressure),
+        yAxisIndex: 2,
+      },
+    ],
+  };
+
+  return <ReactECharts option={option} style={{ height: "400px" }} />
+
+}
+
+export default WeatherChart;
